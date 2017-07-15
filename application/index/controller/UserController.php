67dda5b9573c59ea4a57cde68e7cb4be07e937a9
chaoweb <1648907448@qq.com>
2017-07-15 19:57:30 +0800

    实现用户搜索

--- application/index/controller/UserController.php
+++ application/index/controller/UserController.php
@@ -2,6 +2,7 @@
 namespace app\index\controller;
 use think\Controller;
 use app\index\model\User;
+use think\Request;
 
 class UserController extends Controller
 {
@@ -10,9 +11,20 @@ class UserController extends Controller
 	 */
 	public function index()
 	{
-		// 声明一个空对象
+		// 获取查询信息
+		$name = input('name');
+		// 获取查询信息
+        $name = Request::instance()->get('name');
+
+        $pageSize = 5;
+
+		// 实例化空对象
 		$User = new User;
-		$users = $User->select();
+		$users = $User->where('name' , 'like' , '%' . $name . '%')->paginate($pageSize,false,[
+				'query'=>[
+					'name' => $name,
+				],
+			]);
 
 		// 向v层传递数据
 		$this->assign('users',$users);
@@ -30,10 +42,34 @@ class UserController extends Controller
 	}
 
 	/**
+	 * 保存信息
+	 */
+	public function save()
+	{
+		echo 'this is save';
+	}
+
+	/**
 	 * 修改用户信息
 	 */
 	public function edit()
 	{
 		return $this->fetch();
+	}
+
+	/**
+	 * 更新信息
+	 */
+	public function update()
+	{
+		echo 'this is update';
+	}
+
+	/**
+	 * 删除学生信息
+	 */
+	public function delete()
+	{
+		echo 'this is delete';
 	}
 }
