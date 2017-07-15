cfd8dce451a7fc1f3676ebe32eb59c89c34062bf
张喜硕 <zxishuo@outlook.com>
2017-07-15 17:16:09 +0800

    zhangxishuo

--- application/index/controller/CourseController.php
+++ application/index/controller/CourseController.php
@@ -5,6 +5,8 @@ namespace app\index\controller;
 use think\Controller;
 use think\Request;
 use app\index\model\Course;
+use app\index\model\Term;
+use app\index\model\Coursetime;
 
 /**
 * 课程管理 张喜硕
@@ -70,9 +72,92 @@ class CourseController extends Controller
     }
 
     public function inquiry(){
-        $Course = new Course;
+
+        if(is_null(Request::instance()->post('Courseid'))){
+            $id = Request::instance()->param('id/d');
+        }
+
+        else {
+            $id = Request::instance()->post('Courseid/d');
+        }
+
+        $Course = Course::get($id);
 
         $this->assign('Course',$Course);
+
+        if(is_null(Request::instance()->post('Termid'))){
+            $map = array();
+
+            $map['state'] = 1;
+
+            $Term = Term::get($map);
+        }
+
+        else {
+            $id = Request::instance()->post('Termid/d');
+
+            $Term = Term::get($id);
+        }
+
+        $this->assign('Term',$Term);
+
+        $this->settable($Course,$Term);
+
+        return $this->fetch();
+    }
+
+    private function settable(&$Course,&$Term){
+        for($row = 1 ; $row <= 5 ; $row ++){
+
+            echo '<tr>';
+
+            for($column = 0 ; $column < 8 ; $column ++){
+
+                if ($column == 0) {
+                    echo '<td>' . '第' . $row . '节' . '</td>';
+                }
+
+                else {
+                    $map = array();
+                    $map['day'] = $column;
+                    $map['knob'] = $row;
+
+                    $map['course_id'] = $Course->id;
+                    $map['term_id'] = $Term->id;
+
+                    $Coursetime = new Coursetime;
+                    $Coursetimes = $Coursetime->where($map)->select();
+
+                    $length = sizeof($Coursetimes);
+
+                    echo '<td>';
+
+                        if($length == 0){
+                            echo '<a class="btn btn-success" href="add">';
+                        }
+
+                        else {
+                            echo '<a class="btn btn-default">';
+
+                            for($temp = 0 ; $temp < $length ; $temp ++){
+                                echo $Coursetimes[$temp]->week;
+                                echo ' ';
+                            }
+
+                            echo '</a>';
+                        }
+
+                    echo '</td>';
+                }
+            }
+
+        echo '</tr>';
+        }
+    }
+
+    public function add(){
+
+        var_dump(Request::instance()->post('courseid'));
 
         return $this->fetch();
     }
