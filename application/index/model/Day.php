ee2d2f3ff63d490daf398f504abf4fdc0fc908ea
张喜硕 <zxishuo@outlook.com>
2017-07-16 17:32:19 +0800

    课程管理功能基本完成

--- application/index/model/Day.php
+++ application/index/model/Day.php
@@ -10,18 +10,16 @@ namespace app\index\model;
 class Day
 {
     
-    function __construct($day = 0){
+    function __construct($day = 0 , $course = 0 , $term = 0){
         $this->Day = $day;
+        $this->Course = $course;
+        $this->Term = $term;
     }
 
     public function getKnobs(){
         $Knobs = [];
         for($temp = 1 ; $temp <= 5 ; $temp ++){
-            $Knob = new Knob();
-            $Knob->Course = $this->Course;
-            $Knob->Term = $this->Term;
-            $Knob->Day = $this->Day;
-            $Knob->Knob = $temp;
+            $Knob = new Knob($temp , $this->Course , $this->Term , $this->Day);
             array_push($Knobs, $Knob);
         }
 
