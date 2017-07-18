d3b6ba8040d71707ee015a450393add9a8fe863b
张喜硕 <zxishuo@outlook.com>
2017-07-18 20:34:57 +0800

    课程验证修改

--- application/index/model/Course.php
+++ application/index/model/Course.php
@@ -11,5 +11,26 @@ use think\Model;
 class Course extends Model
 {
     
+    public function checkName() {
+        if(is_null($this->name)) {
+            return false;
+        }
+
+        if(sizeof($this->name)>20) {
+            return false;
+        }
+
+        $Courses = Course::all();
+
+        $Length = sizeof($Courses);
+
+        for($temp = 0 ; $temp < $Length ; $temp ++) {
+            if($Courses[$temp]->name == $this->name) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 }
