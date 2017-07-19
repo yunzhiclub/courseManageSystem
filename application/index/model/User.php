afcc322babc6550c5ebbcaa6ee96356b1134b565
chaoweb <1648907448@qq.com>
2017-07-19 19:29:18 +0800

    找回代码

--- application/index/model/User.php
+++ application/index/model/User.php
@@ -14,27 +14,44 @@ class User extends Model
         // $User要么是一个对象，要么是null。
         if (!is_null($User)) {
             // 验证密码是否正确
-            if ($User->getData('password') !== $password) 
-            {
-                // 用户名密码错误，跳转到登录界面。
-                return 2;
-            } 
-            else 
-            {
+            if ($User->getData('password') === $password) {
+            
                 // 用户名密码正确，将UserId存session。
                 session('username', $User->getData('username'));
                 if ($User->getData('power') == 0)
                     return 0;
                 else if ($User->getData('power') == 1)
                     return 1;
-                else
-                    return 2;
-            }
-            
+            }            
         } 
-        else 
-        {
-            return 2;
+
+        return 2;
+    }
+
+    /**
+     * 判断用户是否登录
+     * @return  bool 登录为true
+     * @author  poshichao
+     * @author  朱晨澍
+     */
+    static public function isLogin()
+    {
+        $username = session('username');
+        $user = User::get($username);
+        $power = $user->power;
+        $pageURL = $_SERVER['PHP_SELF'];
+        if($pageURL=='/courseManageSystem/public/index.php/index/ask_leave/index.html' or $pageURL=='/courseManageSystem/public/index.php/index/ask_leave/'){
+            if($power==0){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            if($power==1){
+                return true;
+            }else{
+                return false;
+            }
         }
     }
 
