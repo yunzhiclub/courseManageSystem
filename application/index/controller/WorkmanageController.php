e3025f4cdd0cb77dc8b53c8170cb26a0da39fa02
zhuchenshu <1369359429@qq.com>
2017-07-21 09:22:18 +0800

    增加出勤管理c层

---
+++ application/index/controller/WorkmanageController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\index\controller;
+
+use app\index\controller\IsloginController;
+
+/**
+* 出勤统计
+* @author 朱晨澍
+*/
+class WorkmamgeController extends IsloginController
+{
+	
+	return $this->fetch();
+}
