569525e40a6cfc2b7ec4e18378670c1c7551b8a3
teacherPan <3792535@qq.com>
2017-11-29 14:19:47 +0800

    重构代码，显示本节及以后的内容

--- application/index/controller/DingController.php
+++ application/index/controller/DingController.php
@@ -10,29 +10,7 @@ class DingController extends Controller {
      * 自动推送钉钉消息方法
      */
     public function push() {
-
-        $ding = new Ding();
-
-        $time = time();
-        $time = $time % 86400;
-
-        // 上午
-        if ($time <= 600) {
-
-            $message = $ding->getMessage('morning');
-            $ding->autoPush($message);
-        }
-        // 下午
-        else if ($time >= 20700 && $time <= 21300) {
-
-            $message = $ding->getMessage('afternoon');
-            $ding->autoPush($message);
-        }
-        // 晚上
-        else if ($time >= 35700 && $time <= 36300) {
-
-            $message = $ding->getMessage('night');
-            $ding->autoPush($message);
-        }
+        // 直接调用M层的方法
+        Ding::pushDingMessage();
     }
 }
