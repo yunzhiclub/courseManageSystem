8a5d555e784730ca3eb73b1bb7b9c27ca73e6ed6
zhangxishuo <zhangxishuo1998@gmail.com>
2018-02-03 17:40:56 +0800

    控制器修改

--- application/index/controller/ContributionController.php
+++ application/index/controller/ContributionController.php
@@ -3,6 +3,7 @@
 namespace app\index\controller;
 
 use think\Controller;
+use app\index\model\User;
 use app\index\model\Contribution;
 
 /**
@@ -12,7 +13,19 @@ use app\index\model\Contribution;
 
 class ContributionController extends Controller {
 
+    /**
+     * 贡献值管理-主页
+     */
     public function index() {
+        $users = User::getUsualUsers();
+        $this->assign('users', $users);
+        return $this->fetch();
+    }
+
+    /**
+     * 解析github数据的接口
+     */
+    public function interface() {
         $json = file_get_contents('php://input');          // 获取传来的json数据
         Contribution::count($json);                        // 调用count方法计数
     }
