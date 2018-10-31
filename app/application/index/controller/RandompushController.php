d30e5949512219a905829cde4823425cb2c39335
moujie1998 <1659411502@qq.com>
2018-10-31 21:58:55 +0800

    格式化代码

--- app/application/index/controller/RandompushController.php
+++ app/application/index/controller/RandompushController.php
@@ -23,21 +23,21 @@ class RandompushController extends IsloginController
 
     // 随机推送
     public function randomPush() {
-    	// 获取所有已选人的名字
-    	$stringArray = Request::instance()->post();
+        // 获取所有已选人的名字
+        $stringArray = Request::instance()->post();
 
-    	// 如果为空，则报错，并返回主页面
-    	if (empty($stringArray)) {
-    		$this->error('请选择要推送的人', url('index'));
-    	}
+        // 如果为空，则报错，并返回主页面
+        if (empty($stringArray)) {
+    	    $this->error('请选择要推送的人', url('index'));
+        }
 
-    	// 随机选择一个人的名字
-    	$randomPosition = array_rand($stringArray["user_name"], 1);
-    	$randomName = $stringArray["user_name"][$randomPosition];
+        // 随机选择一个人的名字
+        $randomPosition = array_rand($stringArray["user_name"], 1);
+        $randomName = $stringArray["user_name"][$randomPosition];
 
-    	// 向钉钉推送消息
-    	$message = '本周随机汇报人是：' . $randomName;
-    	Ding::pushRandomUserName($message);
+        // 向钉钉推送消息
+        $message = '本周随机汇报人是：' . $randomName;
+        Ding::pushRandomUserName($message);
 
         // 发送成功，返回主页面
     	return $this->success('推送成功', url('index'));
